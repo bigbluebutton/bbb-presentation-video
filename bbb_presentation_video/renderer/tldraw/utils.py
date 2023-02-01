7668883a0e28ccbc1303f8c923cbe68374d94810
Calvin Walton <calvin.walton@blindsidenetworks.com>
2023-02-01 14:33:29 -0500

    Fix triangle_centroid method return type

--- bbb_presentation_video/renderer/tldraw/utils.py
+++ bbb_presentation_video/renderer/tldraw/utils.py
@@ -290,7 +290,7 @@ def triangle_centroid(w: float, h: float) -> Tuple[float, float]:
         [w, h],
         [0, h],
     ]
-    return [
+    return (
         (points[0][0] + points[1][0] + points[2][0]) / 3,
         (points[0][1] + points[1][1] + points[2][1]) / 3,
-    ]
+    )
