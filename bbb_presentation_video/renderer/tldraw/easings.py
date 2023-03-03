7a24d62adc6ca9908d38ed318bf479f12dd730b3
Calvin Walton <calvin.walton@blindsidenetworks.com>
2023-03-03 13:41:27 -0500

    Update copyright text in easings, intersect.

--- bbb_presentation_video/renderer/tldraw/easings.py
+++ bbb_presentation_video/renderer/tldraw/easings.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2021 Stephen Ruiz Ltd
-# SPDX-FileCopyrightText: 2022 Calvin Walton
+# SPDX-FileCopyrightText: 2022 BigBlueButton Inc. and by respective authors
 #
 # SPDX-License-Identifier: MIT
 
