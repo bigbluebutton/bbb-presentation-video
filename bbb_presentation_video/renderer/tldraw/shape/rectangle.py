a8708c120aeeab10b204341f5a152baa78e2081b
danielpetri1 <daniel.petri@tum.de>
2024-04-08 20:28:49 +0200

    Fix typo in the year

--- bbb_presentation_video/renderer/tldraw/shape/rectangle.py
+++ bbb_presentation_video/renderer/tldraw/shape/rectangle.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 4 BigBlueButton Inc. and by respective authors
+# SPDX-FileCopyrightText: 2024 BigBlueButton Inc. and by respective authors
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
