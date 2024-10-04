e9f0c2ecf197f2010e908dd483f64d9a4ea84af2
Calvin Walton <calvin.walton@blindsidenetworks.com>
2024-10-04 12:26:45 -0400

    Add poll title, improve style somewhat
    Poll now uses the colors from tldraw for the
    background, which makes it look more like the
    poll in the live meeting. Text size and padding
    is adjusted slightly smaller, since the tldraw
    shapes end up being kind of small for the amount
    of text in them.
    
    Poll title is rendered at the top of the box if
    present.

--- bbb_presentation_video/renderer/tldraw/shape/poll.py
+++ bbb_presentation_video/renderer/tldraw/shape/poll.py
@@ -11,15 +11,17 @@ from gi.repository import Pango, PangoCairo
 
 from bbb_presentation_video.events.helpers import Color
 from bbb_presentation_video.renderer.tldraw.shape import PollShape, apply_shape_rotation
+from bbb_presentation_video.renderer.tldraw.utils import (
+    V2_COLORS,
+    V2_TEXT_COLOR,
+    ColorStyle,
+)
 
 FONT_FAMILY = "Arial"
-POLL_BAR_COLOR = Color.from_int(0x333333)
 POLL_LINE_WIDTH = 2.0
-POLL_FONT_SIZE = 22
-POLL_BG = Color.from_int(0xFFFFFF)
-POLL_FG = Color.from_int(0x000000)
-POLL_VPADDING = 20.0
-POLL_HPADDING = 10.0
+POLL_FONT_SIZE = 18
+POLL_VPADDING = 8.0
+POLL_HPADDING = 8.0
 
 CairoSomeSurface = TypeVar("CairoSomeSurface", bound=cairo.Surface)
 
@@ -36,6 +38,7 @@ def finalize_poll(
 
     width = shape.size.width
     height = shape.size.height
+    color = V2_COLORS.get(shape.style.color, V2_COLORS[ColorStyle.BLACK])
 
     ctx.set_line_join(cairo.LINE_JOIN_MITER)
     ctx.set_line_cap(cairo.LINE_CAP_SQUARE)
@@ -48,9 +51,9 @@ def finalize_poll(
     ctx.line_to(width - half_lw, height - half_lw)
     ctx.line_to(half_lw, height - half_lw)
     ctx.close_path()
-    ctx.set_source_rgb(*POLL_BG)
+    ctx.set_source_rgb(*color.semi)
     ctx.fill_preserve()
-    ctx.set_source_rgb(*POLL_FG)
+    ctx.set_source_rgb(*color.solid)
     ctx.stroke()
 
     font = Pango.FontDescription()
@@ -84,13 +87,31 @@ def finalize_poll(
     max_label_width = min(max_label_width, width * 0.3)
     max_percent_width = min(max_percent_width, width * 0.3)
 
-    bar_height = (height - POLL_VPADDING) / len(shape.answers) - POLL_VPADDING
+    title_height = 0.0
+    if shape.questionText != "":
+        title_height = POLL_FONT_SIZE + POLL_VPADDING
+
+    bar_height = (height - POLL_VPADDING - title_height) / len(
+        shape.answers
+    ) - POLL_VPADDING
     bar_width = width - 4 * POLL_HPADDING - max_label_width - max_percent_width
     bar_x = 2 * POLL_HPADDING + max_label_width
 
     # All sizes are calculated, so draw the poll
+    layout.set_ellipsize(Pango.EllipsizeMode.END)
+    if shape.questionText != "":
+        layout.set_width(int(width - 2 * POLL_HPADDING) * Pango.SCALE)
+        layout.set_text(shape.questionText, -1)
+        title_width, title_height = layout.get_pixel_size()
+        ctx.move_to(
+            (width - title_width) / 2,
+            (POLL_FONT_SIZE - title_height) / 2 + POLL_VPADDING,
+        )
+        ctx.set_source_rgb(*V2_TEXT_COLOR)
+        PangoCairo.show_layout(ctx, layout)
+
     for i, answer in enumerate(shape.answers):
-        bar_y = (bar_height + POLL_VPADDING) * i + POLL_VPADDING
+        bar_y = (bar_height + POLL_VPADDING) * i + POLL_VPADDING + title_height
         if shape.numResponders > 0:
             result_ratio = float(answer.numVotes) / float(shape.numResponders)
         else:
@@ -108,13 +129,12 @@ def finalize_poll(
         )
         ctx.line_to(bar_x + half_lw, bar_y + bar_height - half_lw)
         ctx.close_path()
-        ctx.set_source_rgb(*POLL_BAR_COLOR)
+        ctx.set_source_rgb(*color.solid)
         ctx.fill_preserve()
         ctx.stroke()
 
         # Draw the label and percentage
-        layout.set_ellipsize(Pango.EllipsizeMode.END)
-        ctx.set_source_rgb(*POLL_FG)
+        ctx.set_source_rgb(*V2_TEXT_COLOR)
         layout.set_width(int(max_label_width * Pango.SCALE))
         layout.set_text(answer.key, -1)
         label_width, label_height = layout.get_pixel_size()
@@ -143,10 +163,10 @@ def finalize_poll(
                 bar_x + (bar_x2 - bar_x - votes_width) / 2,
                 bar_y + (bar_height - votes_height) / 2,
             )
-            ctx.set_source_rgb(*POLL_BG)
+            ctx.set_source_rgb(*color.semi)
             PangoCairo.show_layout(ctx, layout)
         else:
             # Votes do not fit in the bar, so put them after
             ctx.move_to(bar_x2 + POLL_HPADDING, bar_y + (bar_height - votes_height) / 2)
-            ctx.set_source_rgb(*POLL_FG)
+            ctx.set_source_rgb(*V2_TEXT_COLOR)
             PangoCairo.show_layout(ctx, layout)
